e98b550854468288bf6f5ea48fb48071650527b7
Bryan Dijkhuizen <dijkhuizenb@outlook.com>
2021-08-03 19:55:06 +0200

    project finished

---
+++ index.js
@@ -0,0 +1,26 @@
+const program = require("commander");
+const chalk = require("chalk");
+const clipboardy = require("clipboardy");
+const createPassword = require("./utils/createPassword");
+const log = console.log;
+program.version("1.0.0").description("Simple Password Genereator");
+
+program
+  .option("-l, --length <number>", "length of password", "8")
+  .option("-s, --save", "save password to passwords.txt")
+  .option("-nn, --no-numbers", "remove numbers")
+  .option("-ns, --no-symbols", "remove symbols")
+
+  .parse();
+
+const { length, save, numbers, symbols } = program.opts();
+
+// Get generated password
+const generatedPassword = createPassword(length, numbers, symbols);
+
+// Copy to clipboard
+clipboardy.writeSync(generatedPassword);
+
+// Ouput generated password
+log(chalk.blue("Generated Password: ") + chalk.bold(generatedPassword));
+log(chalk.yellow("Password copied to clipboard"));
